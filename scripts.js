b6416e1e36d192602372528bf4da6b3e8cdf6290
thanhnguyen98 <ngtruongthanh98@gmail.com>
2022-04-14 09:53:41 +0700

    Hamburger menu for mobile screen

--- scripts.js
+++ scripts.js
@@ -6,3 +6,13 @@ elements.forEach((ele) => {
         this.classList.add("active");
     });
 });
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
